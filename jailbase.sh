9175166283af8e76ed138980270ecadc876f61e9
User Phryk <phryk@commander1024.de>
2017-11-12 21:25:56 +0100

    more complete jailbase, using base.txz instead of make installworld

--- jailbase.sh
+++ jailbase.sh
@@ -1,12 +1,16 @@
-zfs destroy -r zroot/jail/base
-zfs destroy -r zroot/jail/skel
+zfs destroy -r root/jail/base
+zfs destroy -r root/jail/skel
 
-zfs create zroot/jail/base
-zfs create zroot/jail/skel
+zfs create root/jail/base
+zfs create root/jail/skel
 
-cd /usr/src
-make installworld DESTDIR=/jail/base
+#cd /usr/src
+#make installworld DESTDIR=/jail/base
 
+echo "Extracting base"
+tar -C /jail/base -xf base.txz
+
+echo "splitting skel from base"
 mkdir /jail/skel/home
 mv /jail/base/etc /jail/skel
 mv /jail/base/usr/local /jail/skel/usr-local
@@ -14,6 +18,21 @@ mv /jail/base/tmp /jail/skel
 mv /jail/base/var /jail/skel
 mv /jail/base/root /jail/skel
 
-mergemaster -t /jail/skel/var/tmp/temproot -D /jail/skel -i
-cd /jail/skel
-rm -R bin boot lib libexec mnt proc rescue sbin sys usr dev
+#mergemaster -t /jail/skel/var/tmp/temproot -D /jail/skel -i
+#cd /jail/skel
+#rm -R bin boot lib libexec mnt proc rescue sbin sys usr dev
+
+echo "setting up symlinks"
+
+cd /jail/base
+mkdir .rw
+ln -s .rw/etc etc
+ln -s .rw/home home
+ln -s .rw/media media
+ln -s .rw/root root
+ln -s .rw/tmp tmp
+ln -s .rw/var var
+cd usr
+ln -s ../.rw/usr-local local
+
+echo "done!"
